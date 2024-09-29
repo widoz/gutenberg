76494c8e552aabe1cfbfdba828db1bb3f3131a5c
guido <dev@guidoscialfa.com>
2024-09-29 19:58:27 +0200

    Add block bindings plugin
    Signed-off-by: guido <dev@guidoscialfa.com>

---
+++ development/plugins/block-binding/patterns/random-image.php
@@ -0,0 +1,20 @@
+<!-- wp:image {
+	"sizeSlug":"large",
+	"linkDestination":"none",
+	"metadata": {
+		"bindings": {
+			"url": {
+				"source": "block-binding/image-source",
+				"args":{}
+			},
+			"title": {
+				"source": "block-binding/image-title",
+				"args":{}
+			}
+		}
+	}
+} -->
+<figure class="wp-block-image size-large">
+	<img src="https://picsum.photos/200" alt="" title="" />
+</figure>
+<!-- /wp:image -->
