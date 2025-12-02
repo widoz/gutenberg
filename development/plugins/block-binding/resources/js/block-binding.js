24985f9d844302b097719f4f7104f56779cf6af8
Guido Scialfa <dev@guidoscialfa.com>
2025-12-02 09:04:47 +0100

    feat: Replace old image and title binding sources with new dynamic `ninja-turtles` and `custom-title` sources, updating the image pattern and removing a custom URL binding.

--- development/plugins/block-binding/resources/js/block-binding.js
+++ development/plugins/block-binding/resources/js/block-binding.js
@@ -1,23 +1,69 @@
 window.wp.domReady( () => {
 	window.wp.blocks.registerBlockBindingsSource( {
-		name: 'block-binding/custom-url',
-		label: 'Custom URL',
+		name: 'block-binding/ninja-turtles',
+		label: 'Ninja Turtles',
 		getValues( { bindings } ) {
-			if ( bindings?.url?.args?.key === 'custom_url' ) {
+			if ( bindings?.url?.args?.url === 'original_ninja_turtles' ) {
 				return {
-					url: 'https://images6.alphacoders.com/135/1351738.jpeg',
+					url: 'https://i1.sndcdn.com/artworks-000157290951-wydoxo-t500x500.jpg',
+				};
+			}
+			if ( bindings?.url?.args?.url === 'movie_2012' ) {
+				return {
+					url: 'https://m.media-amazon.com/images/I/91OWuXWQQnL._UF1000,1000_QL80_.jpg',
 				};
 			}
 
-			return undefined;
+			return {
+				url: bindings.url,
+			};
 		},
 		getFieldsList() {
-			return {
-				custom_url: {
-					label: 'Custom Url',
+			return [
+				{
+					label: 'Original Ninja Turtles',
 					type: 'string',
+					args: {
+						url: 'original_ninja_turtles',
+					},
 				},
+				{
+					label: '2012 Movie',
+					type: 'string',
+					args: {
+						url: 'movie_2012',
+					},
+				},
+			];
+		},
+		canUserEditValue() {
+			return true;
+		},
+	} );
+	window.wp.blocks.registerBlockBindingsSource( {
+		name: 'block-binding/custom-title',
+		label: 'Custom Title',
+		getValues( { bindings } ) {
+			if ( bindings?.alt?.args?.title === 'custom_title' ) {
+				return {
+					alt: 'Custom Title',
+				};
+			}
+
+			return {
+				alt: bindings.alt,
 			};
+		},
+		getFieldsList() {
+			return [
+				{
+					label: 'Custom Title',
+					type: 'string',
+					args: {
+						title: 'custom_title',
+					},
+				},
+			];
 		},
 		canUserEditValue() {
 			return true;
