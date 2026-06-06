97879d68a2a8f09a7f7ef01b5e3191518dce4f57
guido <dev@guidoscialfa.com>
2026-06-06 20:26:24 +0200

    Add WP Build Experiments Plugin

---
+++ development/plugins/wp-build/packages/client-package-lib/src/index.tsx
@@ -0,0 +1,11 @@
+import { Stack } from '@wordpress/ui';
+
+export function ClientComponent() {
+	return (
+		<Stack>
+			<span>Code</span>
+			<span>is</span>
+			<span>Poetry</span>
+		</Stack>
+	);
+}
